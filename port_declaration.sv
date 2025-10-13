28aa4b6ad60fd154ab020a949d889802d2e0b776
Mantis-1796 <71304802+Mantis-1796@users.noreply.github.com>
2025-10-13 10:14:35 +0530

    Create port_declaration.sv
    Descibes the types of port declarations in modules

---
+++ port_declaration.sv
@@ -0,0 +1,18 @@
+TYPE : 1
+module half_adder(input wire clk,
+                  input wire rst,
+                  input wire a,
+                  input wire b,
+                  output reg sum);
+TYPE : 2
+module half_adder(input clk,
+                  input rst,
+                  input a,
+                  input b,
+                  output reg sum);
+TYPE : 3
+module half_adder(clk,rst,a,b,sum);
+    input clk;
+    input rst;
+    input a,b;
+    output reg sum;
