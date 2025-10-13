e5ae509a5f4dadfe9c5fae1d32695ccf2fb210ce
Mantis-1796 <71304802+Mantis-1796@users.noreply.github.com>
2025-10-13 10:19:45 +0530

    Update port_declaration.sv

--- port_declaration.sv
+++ port_declaration.sv
@@ -16,3 +16,10 @@ module half_adder(clk,rst,a,b,sum);
     input rst;
     input a,b;
     output reg sum;
+
+NOTE:
+  1. TYPE : 1 and TYPE : 2 are exactly the same.
+  2. Wire need not be explicitly defined for inputs.
+  3. TYPE : 3 is old style (Pre -2001 verilog).
+  4. Instead of reg/wire SV supports using logic keyword
+  
