7d604f890230b7c4d9af82fe26f43f2793437c52
Mantis-1796 <71304802+Mantis-1796@users.noreply.github.com>
2025-10-15 15:24:16 +0530

    Update uvm_config_db.sv

--- uvm_config_db.sv
+++ uvm_config_db.sv
@@ -3,5 +3,12 @@ It is used to store and retrieve configuration information(like parameters, hand
 The configuration database in UVM is designed to work top-down, not bottom-up. set at top levels of hierarchy and retrieve at bottom levels. viceversa is not possible.
 
 class uvm_config_db #(type T = uvm_object);
+  
 It’s a parameterized class — meaning it can hold any type (int, bit, string, uvm_object, etc.).
 
+🪣 Basic Syntax
+1️⃣ Setting a configuration
+uvm_config_db#(type)::set(uvm_component cntxt,
+                          string inst_name,
+                          string field_name,
+                          type value);
